39759ce8c8cf556694ad118fcac061c24acf8858
sofia <sofiadere812@gmail.com>
2023-03-23 21:52:10 +0200

    dz

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hello")
+import datetime
+
+class Book():
+
+    def __init__(self, name, author, year):
+        self.name = name
+        self.author = author
+        self.year = year
+
+
+
+    def book_age(self):
+        today = datetime.date.today()
+        age = today.year - self.year
+        print("The book '{0}' is {1} years old.".format(self.name, age))
+
+    def get_into(self):
+        return f'Names book = {self.name}, author = {self.author}, Year of publication = {self.year}.'
+
+
+
+b = Book('The Picture of Dorian Gray', 'Oscar Wilde', 1890)
+
+print(b.get_into())
+print(b.book_age())
